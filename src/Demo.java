62e28450ff03f58d7f8c4c9edfc1297a963bb6e4
pratik8888 <sherdiwala27@gmail.com>
2022-08-08 21:26:44 +0530

    printName method added/implemented

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,12 @@
 public class Demo {
     //?Pratik Sherdiwala (Codelytics)
+
+    static void printName(String name) {
+        System.out.println("Selected Name :" + name);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello");
+        printName("Java First commit using Git");
     }
 }
