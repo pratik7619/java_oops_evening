050c3b31720c5e452583b762485c33b3490f5684
pratik8888 <sherdiwala27@gmail.com>
2022-09-05 22:19:26 +0530

    Singleton class

---
+++ src/access_specifier/encapsulation/SignletonDesignPattern.java
@@ -0,0 +1,52 @@
+package access_specifier.encapsulation;
+
+class Netflix {
+
+    //Global constant
+    private final static Netflix netflix = new Netflix();
+
+    private Netflix() {
+        System.out.println("I am constructor");
+    }
+
+    public static Netflix getInstance() {
+        return netflix;
+    }
+
+    public void watchMovie(String movieName) {
+        //movieName | Singham
+        System.out.println("Watching Movie: " + movieName);
+    }
+}
+
+
+public class SignletonDesignPattern {
+    //?Pratik Sherdiwala (Codelytics)
+    /*
+     ! Singleton Class
+     ============================================================================
+     * The main purpose of Singleton Design Pattern is throughout the project
+     * only one instance will be created.
+     *
+     * Steps to create Singleton Class.
+     *
+     * 1. Constructor should be private.
+     * 2. Create a static getter method to access instance of that class.
+     * 3. Create global static final object in Singleton class.
+     * */
+    public static void main(String[] args) {
+
+        Netflix pratik = Netflix.getInstance();
+        pratik.watchMovie("Singham");
+        System.out.println("Address :" + pratik);//Netflix@123db
+
+        System.out.println("----------------------------");
+
+        Netflix deep = Netflix.getInstance();
+        deep.watchMovie("Hit");
+        System.out.println("Address :" + deep);//Netflix@456db
+        System.out.println("----------------------------");
+
+        TestPerson.main(args);
+    }
+}
--- src/access_specifier/encapsulation/TestPerson.java
+++ src/access_specifier/encapsulation/TestPerson.java
@@ -4,15 +4,19 @@ public class TestPerson {
     //?Pratik Sherdiwala (Codelytics)
     public static void main(String[] args) {
 
-        Person person = new Person();
-        System.out.println(person.getAge());
+//        Person person = new Person();
+//        System.out.println(person.getAge());
+//
+//        person.setAge(50);
+//
+//        System.out.println(person.getAge());//
+//
+//        System.out.println(person.getName());
+//        person.setName("Sona");
+//        System.out.println(person.getName());
 
-        person.setAge(50);
+        Netflix netflix = Netflix.getInstance();
+        System.out.println(netflix);
 
-        System.out.println(person.getAge());//
-
-        System.out.println(person.getName());
-        person.setName("Sona");
-        System.out.println(person.getName());
     }
 }
